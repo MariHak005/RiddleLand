b993a87918e4965dc419659d9ff58418fc0bf81b
MariHak005 <mari_hakobyan@edu.aua.am>
2026-05-10 19:49:27 +0400

    Restore complete scene with player animations

--- Assets/_Scripts/PlayerAnimation.cs
+++ Assets/_Scripts/PlayerAnimation.cs
@@ -10,11 +10,8 @@ public class PlayerAnimation : MonoBehaviour
         float z = Input.GetAxisRaw("Vertical");
 
         float speed = Mathf.Abs(x) + Mathf.Abs(z);
-        bool isMoving = speed > 0.1f;
-        bool isRunning = isMoving && Input.GetKey(KeyCode.LeftShift);
 
         animator.SetFloat("Speed", speed);
-        animator.SetBool("IsRunning", isRunning);
 
         if (Input.GetButtonDown("Jump"))
         {
