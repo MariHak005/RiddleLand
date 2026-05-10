241a2c858cb972099bb8165904cb49d7475be6e5
Anna <annamrteyan390@gmail.com>
2026-05-10 17:45:50 +0400

    Added colored chests, riddles, and win/lose logic

--- Assets/_Scripts/ChestTrigger.cs
+++ Assets/_Scripts/ChestTrigger.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections.Generic;
 
 public class ChestTrigger : MonoBehaviour
 {
@@ -9,21 +11,36 @@ public class ChestTrigger : MonoBehaviour
 
     public GameObject ChestUI;
     public GameObject wrongAnswer;
+    public GameObject correctAnswer;
+    public GameObject SpecialKeysPanel;
     public AudioClip openSound;
+
+    public TextMeshProUGUI RiddleText;
+    public TextMeshProUGUI btnAText;
+    public TextMeshProUGUI btnBText;
+    public TextMeshProUGUI btnCText;
+    public TextMeshProUGUI btnDText;
+    public TextMeshProUGUI keyCountText;
+    public static int specialKeyCount = 0;
+    private bool isOpening = false;
+
+    public List<Riddle> riddleList;
+    private Riddle currentRiddle;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
         audioSource = GetComponent<AudioSource>();
-
         ChestUI.SetActive(false);
         wrongAnswer.SetActive(false);
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !isOpening) 
         {
+            isOpening = true; // Close the gate
             animator.SetBool("isOpen", true);
             audioSource.PlayOneShot(openSound);
             StartCoroutine(ShowUI());
@@ -33,26 +50,103 @@ public class ChestTrigger : MonoBehaviour
     private IEnumerator ShowUI()
     {
         yield return new WaitForSeconds(2f);
-        ChestUI.SetActive(true);
+        if (ChestUI != null) ChestUI.SetActive(true);
+        if (SpecialKeysPanel != null) SpecialKeysPanel.SetActive(false);
+        DisplayRandomRiddle();
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+
+    void DisplayRandomRiddle()
+    {
+        if (riddleList.Count > 0)
+        {
+            int randomIndex = Random.Range(0, riddleList.Count);
+            currentRiddle = riddleList[randomIndex];
+
+            RiddleText.text = currentRiddle.question;
+            btnAText.text = currentRiddle.optionA;
+            btnBText.text = currentRiddle.optionB;
+            btnCText.text = currentRiddle.optionC;
+            btnDText.text = currentRiddle.optionD;
+            
+        }
     }
 
     public void AnswerA()
     {
-        Wrong();
+        if (currentRiddle.correctAnswerIndex == 0) Win(); 
+        else Wrong();
     }
 
     public void AnswerB()
     {
-        Wrong();
+        if (currentRiddle.correctAnswerIndex == 1) Win();
+        else Wrong();
     }
 
     public void AnswerC()
     {
-        SceneManager.LoadScene("Level2");
+        if (currentRiddle.correctAnswerIndex == 2) Win(); 
+        else Wrong();
+    }
+    public void AnswerD() 
+    {
+        if (currentRiddle.correctAnswerIndex == 3) Win();
+        else Wrong();
     }
 
+    void Win()
+    {
+        if (correctAnswer != null)
+        {
+            correctAnswer.SetActive(true);
+        }
+
+        specialKeyCount++;
+        if (keyCountText != null)
+        {
+            keyCountText.text = "Special Keys: " + specialKeyCount;
+        }
+
+        ChestUI.SetActive(false);
+        if (SpecialKeysPanel != null) SpecialKeysPanel.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        StartCoroutine(HideWinPanel());
+        enabled = false;
+    }
+
+    private IEnumerator HideWinPanel()
+    {
+        yield return new WaitForSeconds(3f);
+        if (correctAnswer != null)
+        {
+            correctAnswer.SetActive(false);
+        }
+    }
     void Wrong()
     {
         wrongAnswer.SetActive(true);
     }
+    public void TryAgain()
+    {
+        wrongAnswer.SetActive(false); 
+        ChestUI.SetActive(true);       
+    }
+}
+
+[System.Serializable]
+public class Riddle
+{
+    public string question;
+    public string optionA;
+    public string optionB;
+    public string optionC;
+    public string optionD;
+    public int correctAnswerIndex;
 }
